c4cb6da1ccbf878ebbba95a9de8c4c152a0758a1
Dongkey <86659102+DongKey777@users.noreply.github.com>
2024-02-19 16:14:09 +0900

    [Silver II] Title: 유기농 배추, Time: 316 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,39 +1,41 @@
 from collections import deque
 
-dx = [0,0,1,-1]
-dy = [1,-1,0,0]
+T = int(input())
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
-t = int(input())
 
 def bfs(graph, a, b):
     queue = deque()
-    queue.append((a,b))
+    queue.append((a, b))
     graph[a][b] = 0
-
     while queue:
         x, y = queue.popleft()
         for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
-            if nx < 0 or nx >=n or ny < 0 or ny >= m:
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= M or ny < 0 or ny >= N or graph[nx][ny] == 0:
                 continue
+
             if graph[nx][ny] == 1:
                 graph[nx][ny] = 0
                 queue.append((nx, ny))
-    return
 
-for i in range(t):
+
+for _ in range(T):
+    M, N, K = map(int, input().split())
+    graph = [[0] * N for j in range(M)]
     cnt = 0
-    n, m, k = map(int,input().split())
-    graph = [[0]*m for _ in range(n)]
 
-    for j in range(k):
-        x, y = map(int, input().split())
-        graph[x][y] = 1
+    for _ in range(K):
+        i, j = map(int, input().split())
+        graph[i][j] = 1
 
-    for a in range(n):
-        for b in range(m):
-            if graph[a][b] == 1:
-                bfs(graph, a, b)
+    for i in range(M):
+        for j in range(N):
+            if graph[i][j] == 1:
                 cnt += 1
+                bfs(graph, i, j)
+
     print(cnt)
