b7b0d154ede5ba528e016f2142d522225bfea9fb
Dongkey <86659102+DongKey777@users.noreply.github.com>
2024-02-21 13:19:42 +0900

    [Silver I] Title: 스타트링크, Time: 516 ms, Memory: 38932 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.py
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -1,36 +1,29 @@
-from collections import deque
-
-F, S, G, U, D = map(int, input().split())
-
-graph = [0] * (F + 1)
-
-
-def bfs(start):
-    queue = deque()
-    queue.append(start)
+def elevator_button_press(F, S, G, U, D):
+    visited = [False] * (F + 1)  # 각 층을 방문했는지 여부를 저장하는 리스트
+    queue = [(S, 0)]  # 현재 층과 누른 버튼 횟수를 저장하는 큐
 
     while queue:
-        floor = queue.popleft()
+        current_floor, button_press = queue.pop(0)
 
-        if graph[G] != 0 or S == G:
-            break
+        if current_floor == G:  # G층에 도착했을 때
+            return button_press
 
-        for i in (U, (-1) * D):
+        up_floor = current_floor + U
+        down_floor = current_floor - D
 
-            new_floor = floor + i
-            if new_floor < 1 or new_floor > F or graph[new_floor] != 0 or new_floor == floor:
-                continue
+        if 1 <= up_floor <= F and not visited[up_floor]:
+            visited[up_floor] = True
+            queue.append((up_floor, button_press + 1))
 
-            graph[new_floor] = graph[floor] + 1
-            queue.append(new_floor)
+        if 1 <= down_floor <= F and not visited[down_floor]:
+            visited[down_floor] = True
+            queue.append((down_floor, button_press + 1))
 
+    return "use the stairs"  # G층에 도착할 수 없는 경우
 
-bfs(S)
-if graph[G] == 0 and G != S:
-    print("use the stairs")
+# 입력 받기
+F, S, G, U, D = map(int, input().split())
 
-elif graph[G] != 0:
-    print(graph[G])
-    
-else:
-    print("0")
+# 결과 출력
+result = elevator_button_press(F, S, G, U, D)
+print(result)
