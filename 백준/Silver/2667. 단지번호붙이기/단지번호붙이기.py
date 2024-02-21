8058a79ab57cc6daf56a79cbc07419a62b02925a
Dongkey <86659102+DongKey777@users.noreply.github.com>
2024-02-21 14:42:41 +0900

    [Silver I] Title: 단지번호붙이기, Time: 64 ms, Memory: 34060 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,37 +1,45 @@
-N = int(input())
+from collections import deque
 
-graph = [list(map(int,input())) for _ in range(N)]
+n = int(input())
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-num = []
-def dfs(x,y):
-    if x<0 or x>=N or y<0 or y>=N:
-        return False
+graph = [list(map(int, list(input()))) for _ in range(n)]
 
-    if graph[x][y] == 1:
-        graph[x][y] = 0
-        global count
-        count += 1
+
+def bfs(a, b):
+    queue = deque()
+    queue.append((a, b))
+    graph[a][b] = 0
+    cnt = 1
+
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+
+    while queue:
+        x, y = queue.popleft()
+
         for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
+            nx = x + dx[i]
+            ny = y + dy[i]
 
-            dfs(nx,ny)
-        return True
-    return False
+            if nx < 0 or nx >= n or ny < 0 or ny >= n:
+                continue
 
-result = 0
-count = 0
+            if graph[nx][ny] != 0:
+                graph[nx][ny] = 0
+                queue.append((nx, ny))
+                cnt += 1
 
-for i in range(N):
-    for j in range(N):
-        if dfs(i,j):
-            num.append(count)
-            result += 1
-            count = 0
+    return cnt
 
-num.sort()
-print(result)
-for i in range(len(num)):
-    print(num[i])
+
+cnt_town = 0
+cnt_house = []
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] != 0:
+            cnt_town += 1
+            cnt_house.append(bfs(i, j))
+
+print(cnt_town)
+for house in sorted(cnt_house):
+    print(house)
