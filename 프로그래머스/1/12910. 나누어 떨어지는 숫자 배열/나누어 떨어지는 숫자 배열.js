05323a87b609566314893eccf0cc9a9992f52b4d
Dongkey <86659102+DongKey777@users.noreply.github.com>
2024-05-14 18:13:34 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
@@ -0,0 +1,8 @@
+function solution(arr, divisor) {
+    var answer = arr.filter((a) => a % divisor == 0).sort((a,b) => a-b);
+
+    if (answer.length == 0) {
+        answer.push(-1);
+    }
+    return answer;
+}
