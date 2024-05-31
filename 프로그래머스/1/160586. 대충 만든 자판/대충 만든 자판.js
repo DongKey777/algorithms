96c4b0c8f3c61895e22b62d42247a50065ae3056
Dongkey <86659102+DongKey777@users.noreply.github.com>
2024-05-31 14:53:56 +0900

    [level 1] Title: 대충 만든 자판, Time: 1.00 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -0,0 +1,29 @@
+function solution(keymap, targets) {
+    
+    var keys = {};
+    
+    for(key of keymap) {
+        for (var idx = 0; idx < key.length; idx++) {
+            if (key[idx] in keys && idx+1 < keys[key[idx]]) {
+                keys[key[idx]] = idx+1;
+            }else if (!(key[idx] in keys)) {
+                keys[key[idx]] = idx+1;
+            }
+        }
+    }
+    
+    var answer = [];
+    for (var target of targets) {
+        var count = 0;
+        for(var ca of [...target]) {
+            if (ca in keys) {
+                count += keys[ca];
+            }else {
+                count = -1;
+                break;
+            }
+        }
+        answer.push(count);
+    }
+    return answer;
+}
